054f8af0b81212cb677b5ddc7c3b903d8dd27e63
Konstantin Kravchenko <konstantinkravchenko@MacBook-KKK.local>
2023-01-15 23:53:05 +0300

    Выделена функция формирования массива имен файлов в соответствии с заданным расширением

--- parse_files.py
+++ parse_files.py
@@ -2,10 +2,11 @@ import csv
 from biom import load_table
 from collections import Counter
 import threading
-import time
+import queue
+from time  import sleep
 import os
 import re
-
+import multiprocessing
 
 # Определим таксон по умолчанию
 TARGET = "s__"
@@ -16,23 +17,24 @@ Q_THREADS = 4
 # Инициализируем количество упоминаний заданного таксона и суммарную SSU rRNA и 
 # составим словарь-счетчик всех упоминаний различных  таксонов во всех файлах и их SSU rRNA
 
-# Quantity_by_taxon = 0
-# Sum_by_taxon = 0
 SSU_rRNA_by_taxon = Counter()
 Number_by_taxon = Counter()
+
+# Обьявляем очередь для передачи данных из потоков в Общие счетчики-словари
+queue_collect_SSU = queue.Queue()
+
 
 # Эта функция выгружает данные из файла biom в обьект класса biom. 
 # Аргументы:
 # 1. Массив полных имен файлов 
 # 2. Имя таксона 
-# Затем методами класса biom проводит фильтрацию по всем таксонам 
-# создает словарь и добавляет словарь этого файла к глобальному словарю-счетчику
+# Затем методами класса biom проводит фильтрацию по всем подходящим таксонам 
+# создает словарь и выгружает словарь этого файла в очередь
 # Функция запускается в отдельных потоках
  
 def parse_biom(arr, taxon):
 
     Dict_SSU_temp = {}
-    Number_taxon_temp = {}
     Name_of_taxon_temp = ''
     SSU_rRNA_by_taxon_temp = 0
 
@@ -48,24 +50,25 @@ def parse_biom(arr, taxon):
             Name_of_taxon_temp = table.metadata(id = i, axis = 'observation').get('taxonomy').split(';')[-1]
             SSU_rRNA_by_taxon_temp = table.data(id = i, axis='observation')[0]
             Dict_SSU_temp[Name_of_taxon_temp] = SSU_rRNA_by_taxon_temp
-            Number_taxon_temp[Name_of_taxon_temp] = 1
 
   
-# При обращении функции к общим переменным происходит блокировка, 
-# чтобы не возникло конфликта между потоками
+# Выгрузка происходит в  очередь
 
-        with lock:
-            global SSU_rRNA_by_taxon, Number_by_taxon
-
-            SSU_rRNA_by_taxon.update(Dict_SSU_temp)
-            Number_by_taxon.update(Number_taxon_temp)
+        queue_collect_SSU.put(Dict_SSU_temp)
 
     return
- 
+
+# Эта функция выгружает данные из файла tsv в массив. 
+# Аргументы:
+# 1. Массив полных имен файлов 
+# 2. Имя таксона 
+# Затем проводит фильтрацию массива по всем подходящим таксонам 
+# создает словарь и выгружает словарь этого файла в очередь 
+# Функция запускается в отдельных потоках
+
 def parse_tsv(arr,taxon):
 
     Dict_SSU_temp = {}
-    Number_taxon_temp = {}
     Name_of_taxon_temp = ''
     SSU_rRNA_by_taxon_temp = 0
 
@@ -89,14 +92,37 @@ def parse_tsv(arr,taxon):
                         Name_of_taxon_temp = row[index_taxonomy].split(';')[-1]
                         SSU_rRNA_by_taxon_temp = float(row[index_SSU_rRNA])
                         Dict_SSU_temp[Name_of_taxon_temp] = SSU_rRNA_by_taxon_temp
-                        Number_taxon_temp[Name_of_taxon_temp] = 1
 
-            with lock:
-                global SSU_rRNA_by_taxon, Number_by_taxon
+            queue_collect_SSU.put(Dict_SSU_temp)
 
-                SSU_rRNA_by_taxon.update(Dict_SSU_temp)
-                Number_by_taxon.update(Number_taxon_temp)
     return
+
+# Функция Collector  забирает информацию из очереди и добавляет ее в соответствующие счетчики
+
+def collector():
+
+    while True:
+
+        dict_temp = queue_collect_SSU.get()
+        SSU_rRNA_by_taxon.update(dict_temp)
+
+        for i in dict_temp : dict_temp[i] = 1
+
+        Number_by_taxon.update(dict_temp)
+
+
+# Функция array_of_files(str) получает в качестве аргумента расширение имен файлов, а возвращает
+#  список имен файлов с этим расширением  из папки ~/data 
+
+def array_of_files(ext_str):
+
+    dir_name = os.getcwd() + "/data"
+    arr_temp = list(filter(lambda  val: ("."+ext_str) in val, os.listdir(dir_name)))
+
+    for i in range(len(arr_temp)):
+        arr_temp[i] = dir_name + "/" + arr_temp[i]
+
+    return arr_temp
 
 if __name__ == "__main__":
 
@@ -104,22 +130,14 @@ if __name__ == "__main__":
 
     taxon = input("Введите имя таксона или нажмите Enter (Имя по умолчанию %s )\n\n" % TARGET)
 
-    
     if taxon == "" : taxon = TARGET
 
-#получаем имена всех файлов в папке ./biom
-    dirname = os.path.dirname(__file__)
-    tree = os.walk(dirname + "/biom/")
 
-# Формируем массив имен файлов для передачи в функцию обработки
+# Получение списка имен файлов biom из папки data
 
-    for i in tree:
-        biom_arr = [*i]
+    biom_arr = array_of_files("biom")   
 
-    Number_of_Files = len(biom_arr[2])
-
-    for i in range(Number_of_Files):
-        biom_arr[2][i] = dirname + "/biom/" + biom_arr[2][i]
+    Number_of_Files = len(biom_arr)
 
 
 #Разделим исходный массив на части для каждого потока
@@ -129,17 +147,21 @@ if __name__ == "__main__":
     biom_arr_by_threads = [[]*Q_THREADS for i in range(Q_THREADS)]
 
     for i in range(Q_THREADS-1):
-        biom_arr_by_threads[i] = biom_arr[2][L_Q_Threads*i:L_Q_Threads*(i+1)]
+        biom_arr_by_threads[i] = biom_arr[L_Q_Threads*i:L_Q_Threads*(i+1)]
 
-    biom_arr_by_threads[Q_THREADS - 1] = biom_arr[2][L_Q_Threads*(Q_THREADS-1) : Number_of_Files]
-        
-# Запускаем метод блокировки для избежания конфликта между потоками при обращении к одним переменным
-    lock = threading.Lock()
+    biom_arr_by_threads[Q_THREADS - 1] = biom_arr[L_Q_Threads*(Q_THREADS-1) : Number_of_Files]
+
 
 # Формируем массив потоков
-    threads = [threading.Thread(target = parse_biom, args = (biom_arr_by_threads[i],taxon,)) for i in range(Q_THREADS)]
+    threads = [threading.Thread(target = parse_biom, args = (biom_arr_by_threads[i],taxon,), daemon=True) for i in range(Q_THREADS)]
+
+# Формируем поток для collector
+
+    thread_collector = threading.Thread(target = collector, daemon=True)
 
 # Старт потоков
+    thread_collector.start()
+
     for thread in threads:
         thread.start()
     
@@ -147,19 +169,11 @@ if __name__ == "__main__":
         thread.join()
 
 
-    #получаем имена всех файлов в папке ./tsv
-    dirname = os.path.dirname(__file__)
-    tree = os.walk(dirname + "/tsv/")
+# Получение списка имен файлов tsv из папки data
 
-# Формируем массив имен файлов для передачи в функцию обработки
+    tsv_arr = array_of_files("tsv")   
 
-    for i in tree:
-        tsv_arr = [*i]
-
-    Number_of_Files = len(tsv_arr[2])
-
-    for i in range(Number_of_Files):
-        tsv_arr[2][i] = dirname + "/tsv/" + tsv_arr[2][i]
+    Number_of_Files = len(tsv_arr)
 
 
 #Разделим исходный массив на части для каждого потока
@@ -170,15 +184,13 @@ if __name__ == "__main__":
     tsv_arr_by_threads = [[]*Q_THREADS for i in range(Q_THREADS)]
 
     for i in range(Q_THREADS-1):
-        tsv_arr_by_threads[i] = tsv_arr[2][L_Q_Threads*i:L_Q_Threads*(i+1)]
+        tsv_arr_by_threads[i] = tsv_arr[L_Q_Threads*i:L_Q_Threads*(i+1)]
 
-    tsv_arr_by_threads[Q_THREADS - 1] = tsv_arr[2][L_Q_Threads*(Q_THREADS-1) : Number_of_Files]
-        
-# Запускаем метод блокировки для избежания конфликта между потоками при обращении к одним переменным
-    lock = threading.Lock()
+    tsv_arr_by_threads[Q_THREADS - 1] = tsv_arr[L_Q_Threads*(Q_THREADS-1) : Number_of_Files]
+
 
 # Формируем массив потоков
-    threads = [threading.Thread(target = parse_tsv, args = (tsv_arr_by_threads[i],taxon,)) for i in range(Q_THREADS)]
+    threads = [threading.Thread(target = parse_tsv, args = (tsv_arr_by_threads[i],taxon,), daemon=True) for i in range(Q_THREADS)]
 
 # Старт потоков
     for thread in threads:
@@ -187,10 +199,9 @@ if __name__ == "__main__":
     for thread in threads:
         thread.join()
 
-
 # Вывод результата обработки в файл
 
-    with open(dirname +'/taxon.csv', 'w', newline='') as csvfile:
+    with open(os.getcwd() +'/taxon.csv', 'w', newline='') as csvfile:
 
         writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
 
